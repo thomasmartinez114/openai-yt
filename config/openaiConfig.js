fab82de5241a04d155be7a6c205987a606628b11
Thomas Martinez <devtommie114@gmail.com>
2024-09-23 16:08:04 -0400

    added tags generator

--- config/openaiConfig.js
+++ config/openaiConfig.js
@@ -1,16 +1,3 @@
-// const { Configuration, OpenAIApi } = require('openai');
-// require('dotenv').config();
-
-// // Create configuration instance
-// const configuration = new Configuration({
-//   apiKey: process.env.OPEN_AI_KEY, // Ensure your API key is set correctly in the .env file
-// });
-
-// // Create OpenAI API instance
-// const openai = new OpenAIApi(configuration);
-
-// module.exports = openai;
-
 const OpenAI = require('openai');
 require('dotenv').config();
 
--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -4,7 +4,7 @@ const openai = require('../config/openaiConfig');
 
 const generateMeta = async title => {
   try {
-    const response = await openai.chat.completions.create({
+    const description = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       messages: [
         {
@@ -12,12 +12,24 @@ const generateMeta = async title => {
           content: `Come up with a description for a YouTube video called "${title}"`,
         },
       ],
+      max_tokens: 100,
+    });
+
+    const tags = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `Come up with 10 keywords for a YouTube video called ${title}`,
+        },
+      ],
+      max_tokens: 100,
     });
 
     // Check the structure of the response
-    const description = response.choices[0].message.content;
-    console.log(description);
-    return description; // Return the description
+    // const description = response.choices[0].message.content;
+    console.log(description.choices[0].message.content);
+    console.log(tags.choices[0].message.content);
   } catch (error) {
     console.error(
       'Error generating description:',
